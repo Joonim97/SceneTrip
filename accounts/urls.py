ed32827b914725a9f3bb825cc615e246aa75cba6
unknown <pigking41@gmail.com>
2024-09-26 15:15:09 +0900

    feat: 로그인 기능 username, password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from . import views
 from django.urls import path
 from .views import SignupAPIView, VerifyEmailAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path("signup/", SignupAPIView.as_view(), name="signup"),
     path('verify/<str:token>/', VerifyEmailAPIView.as_view(), name='verify_email'),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair") # 로그인로직
     ]
