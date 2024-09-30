b104622a52742113a3b507de097e31b1a6f56b12
HeureuseD <da4260@daum.net>
2024-09-30 15:36:43 +0900

    feat: 촬영지 상세조회 url 추가

--- locations/urls.py
+++ locations/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LocationListAPIView, LocationSearchAPIView
+from .views import LocationListAPIView, LocationSearchAPIView, LocationDetailAPIView, LocationsaveView
 
 app_name = "locations"
 
@@ -7,7 +7,7 @@ urlpatterns = [
     path("lists/", LocationListAPIView.as_view(), name="location-list"),
     path("search/", LocationSearchAPIView.as_view(), name="location-search"),
     # path("lists/<str:지역명>/"),
-    # path("lists/<int:pk>"),
-    # path("<int:pk>/saves/"),
-    # path("lists/plans"),
+    path("lists/<int:pk>/", LocationDetailAPIView.as_view(), name="location-detail"),
+    # path("<int:pk>/saves/", LocationsaveView.as_view(), name="location-save"),
+    # path("lists/plans/"),
 ]
