12ee26fb28b60c831a39782085b9b12ed45d9a94
HeureuseD <da4260@daum.net>
2024-10-01 00:56:12 +0900

    feat: LocationRegionAPIView(촬영지 지역별 조회) url 작성

--- locations/urls.py
+++ locations/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import LocationListAPIView, LocationSearchAPIView, LocationDetailAPIView, LocationSaveView
+from .views import LocationListAPIView, LocationSearchAPIView, LocationRegionAPIView, LocationDetailAPIView, LocationSaveView
 
 app_name = "locations"
 
 urlpatterns = [
     path("lists/", LocationListAPIView.as_view(), name="location-list"),
     path("search/", LocationSearchAPIView.as_view(), name="location-search"),
-    # path("lists/<str:지역명>/"),
+    path("lists/<str:region>/", LocationRegionAPIView.as_view(), name="location-region-list"),
     path("lists/<int:pk>/", LocationDetailAPIView.as_view(), name="location-detail"),
     path("<int:id>/saves/", LocationSaveView.as_view(), name="location-save"),
     # path("lists/plans/"),
