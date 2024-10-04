2dd32079e4bd751ef8310e1091cd42b659fd4a5b
unknown <pigking41@gmail.com>
2024-10-04 11:35:31 +0900

    Add: 저널에 좋아요 수 보이게 추가

--- journals/serializers.py
+++ journals/serializers.py
@@ -47,16 +47,21 @@ class CommentLikeSerializer(serializers.ModelSerializer):
 class JournalImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = JournalImage
-        fields = ['id', 'image']  # 이미지 필드만 포함
+        fields = ['id', 'journal_image']  # 이미지 필드만 포함
 
+# 
 class JournalSerializer(serializers.ModelSerializer):
-    user_nickname = serializers.ReadOnlyField(source='user.nickname')  # 사용자 닉네임 읽기 전용 필드
+    likes_count= serializers.IntegerField(source='likes.count', read_only=True) # 좋아요 수 조회
+    author_nickname = serializers.ReadOnlyField(source='author.nickname')  # 사용자 닉네임 읽기 전용 필드
     journal_images = JournalImageSerializer(many=True, read_only=True)  # 다중 이미지 시리얼라이저
 
     class Meta:
         model = Journal
-        fields = '__all__'  # 모든 필드 포함
-        read_only_fields = ('user_nickname', 'user', 'created_at', 'updated_at', 'hit_count')  # 읽기 전용 필드
+        fields = ['id','title','content','author','created_at','likes_count','author_nickname','journal_images','hit_count']  # 보이는 필드들
+        read_only_fields = ('likes','likes_count','author_nickname', 'author', 'created_at', 'updated_at', 'hit_count')  # 읽기 전용 필드
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
 
 
 
