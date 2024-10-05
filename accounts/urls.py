415688dfc67d2aced609651f348a0742c3795633
unknown <pigking41@gmail.com>
2024-10-06 02:10:20 +0900

    feat: 회원탈퇴 엔드포인트 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 from .views import (SignupAPIView, VerifyEmailAPIView, LogoutAPIView, SubscribeView, Mypage, PasswordResetRequestView,
                     PasswordResetConfirmView, EmailResetRequestView,
-                    EamilResetConfirmView, MyJournalsListAPIView, SavedLocationsListAPIView, SubscribingsListAPIView, SubsribingsjournalAPI, MyCommunityListAPIView)
+                    EamilResetConfirmView, MyJournalsListAPIView, SavedLocationsListAPIView, SubscribingsListAPIView, SubsribingsjournalAPI, MyCommunityListAPIView, DeleteAPIView)
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 app_name = 'accounts'
@@ -24,4 +24,5 @@ urlpatterns = [
     path('<str:nickname>/mypage/subscribings/', SubscribingsListAPIView.as_view(), name='subscribings'), # 내가 구독한 사람 전체보기
     path('<str:nickname>/mypage/communities_author/', MyCommunityListAPIView.as_view(), name='my_journals'), # 내가 쓴 글 전체보기
     path('<str:nickname>/mypage/<str:sub_nickname>/', SubsribingsjournalAPI.as_view(), name='subscribings_journal'), # 내가 구독한 인원 글 보기
+    path('<str:nickname>/delete/', DeleteAPIView.as_view(), name='accounts_delete')
 ]
