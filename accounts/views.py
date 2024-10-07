06d088343051eace91fc7f266792d89359ed4da3
unknown <pigking41@gmail.com>
2024-10-07 09:54:14 +0900

    Update: 코드 보완

--- accounts/views.py
+++ accounts/views.py
@@ -104,11 +104,11 @@ class Mypage(ListAPIView): # 마이 페이지
             profile_image = request.FILES['profile_image']
             user.profile_image = profile_image 
         elif 'profile_image' in request.data and not request.data['profile_image']:
-            # profile_image가 없으면 제거
             user.profile_image = None
 
         user.save()  # 변경 사항 저장
         return Response({"message": "프로필 정보가 업데이트되었습니다."}, status=status.HTTP_200_OK)
+
 
 class SubscribeView(APIView):  # 구독 기능
     permission_classes = [IsAuthenticated]
@@ -129,32 +129,6 @@ class SubscribeView(APIView):  # 구독 기능
                 return Response("구독했습니다.", status=status.HTTP_200_OK)
             else:
                 return Response("자신의 계정은 구독할 수 없습니다.", status=status.HTTP_200_OK)
-
-# # password 리셋
-# class PasswordResetRequestView(APIView):
-#     def post(self, request):
-#         try:
-#             email = request.data.get('email')
-#         except:
-#             return Response({"error": "이메일을 입력해주십시오."}, status=404)
-#         user = User.objects.filter(email=email).first()
-#         try:
-#             if user:
-#                 uid = urlsafe_base64_encode(force_bytes(user.pk))
-#                 token = default_token_generator.make_token(user)
-#                 reset_url = f"{request.scheme}://{request.get_host()}/reset/{uid}/{token}/"
-#                 # message = f"안녕하세요 {user.username}님,\n\n비밀번호 재설정을 위해 아래 링크를 클릭하세요:\n{reset_url}\n\n감사합니다."
-#                 message = f'uid: {uid}  |  token: {token}'
-#                 send_mail(
-#                     '비밀번호 변경 요청',
-#                     message,
-#                     'noreply@gmail.com',
-#                     [user.email],
-#                     fail_silently=False,
-#                 )
-#             return Response({"message": "해당 이메일을 사용하는 계정이 있는 경우, 비밀번호 재설정 메일을 전송합니다."}, status=status.HTTP_200_OK)
-#         except:
-#             return Response({"error": "오류가 발생하였습니다."}, status=404)
 
 # 비밀번호 리셋 로직
 class PasswordResetRequestView(APIView):
@@ -189,28 +163,7 @@ class PasswordResetRequestView(APIView):
         
         except:
             return Response({"error": "사용자를 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND)
-    
-# # password 재설정
-# class PasswordResetConfirmView(APIView):
-#     def post(self, request, uidb64, token):
-#         uid = force_str(urlsafe_base64_decode(uidb64))
-#         user = User.objects.get(pk=uid)
-#         if not default_token_generator.check_token(user, token):
-#             return Response({"error": "토큰값을 입력해주세요."}, status=status.HTTP_400_BAD_REQUEST)
-        
-#         serializer = PasswordCheckSerializer(data=request.data)
-#         try:
-#             if serializer.is_valid():
-#                 # 유효성 검사를 통과한 데이터에서 비밀번호를 가져오기
-#                 new_password = serializer.validated_data['new_password']
-#                 user.set_password(new_password)
-#                 user.save()
-#                 return Response({"message": "비밀번호가 변경되었습니다."}, status=status.HTTP_200_OK)
-#             else:
-#                 # 유효성 검사를 통과하지 못하면 오류 반환
-#                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#         except:
-#             return Response({"error": "오류가 발생하였습니다."}, status=404)
+
 
 # 이메일 인증 메일이 날아올 경우
 class PasswordResetConfirmView(APIView):
@@ -228,9 +181,7 @@ class PasswordResetConfirmView(APIView):
             return HttpResponse({'error':'비밀번호 변경이 정상적으로 처리되지 않으셨습니다.'}, status=status.HTTP_400_BAD_REQUEST)    
 
 
-
-
-# 이메일 리셋 로직
+# 이메일 리셋
 class EmailResetRequestView(APIView):
     permission_classes = [IsAuthenticated]  # 로그인된 사용자만 가능
 
@@ -263,7 +214,6 @@ class EmailResetRequestView(APIView):
 # 이메일 인증 메일이 날아올 경우
 class EamilResetConfirmView(APIView):
     def get(self, request, token):
-        # 예외처리 해서 만약 안될경우 서버 안터지게
         try:
             user = get_object_or_404(User, verification_token=token)
             user.email = user.new_email
@@ -295,10 +245,9 @@ class SavedLocationsListAPIView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, nickname):
-        try:
-            user = get_object_or_404(User, nickname=nickname)  # 닉네임으로 사용자 조회
-        except:
-            return Response({"error": "사용자를 찾을수 없습니다."}, status=404)
+
+        user = get_object_or_404(User, nickname=nickname)  # 닉네임으로 사용자 조회
+
         if user == request.user:  # 요청한 사용자가 본인인지 확인
             saved_locations = user.location_save.all()  # 사용자의 모든 저장된 촬영지 가져오기
             serializer = LocationSaveSerializer(saved_locations, many=True)
@@ -311,10 +260,9 @@ class SubscribingsListAPIView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, nickname):
-        try:
-            user = get_object_or_404(User, nickname=nickname)  # 닉네임으로 사용자 조회
-        except:
-            return Response({"error": "사용자를 찾을수 없습니다."}, status=404)
+
+        user = get_object_or_404(User, nickname=nickname)  # 닉네임으로 사용자 조회
+
         if user == request.user:  # 요청한 사용자가 본인인지 확인
             subscribings = user.subscribings.all()  # 사용자의 모든 구독 가져오기
             serializer = SubUsernameSerializer(subscribings, many=True)
@@ -327,11 +275,9 @@ class SubsribingsjournalAPI(generics.ListAPIView):
     permission_classes = [IsAuthenticated] 
 
     def get(self, request, nickname, sub_nickname):
-        try:
-            user = get_object_or_404(User, nickname=nickname) # 닉네임으로 사용자 조회
-            sub_user = get_object_or_404(User, nickname=sub_nickname) # 구독한 사용자를 조회
-        except:
-            return Response({"error": "사용자를 찾을수 없습니다."}, status=400)
+            
+        user = get_object_or_404(User, nickname=nickname) # 닉네임으로 사용자 조회
+        sub_user = get_object_or_404(User, nickname=sub_nickname) # 구독한 사용자를 조회
         
         if user.subscribings.filter(nickname=sub_nickname).exists(): 
         # 구독한 사용자가 작성한 저널들 가져오기
@@ -346,10 +292,9 @@ class MyCommunityListAPIView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, nickname):
-        try:
-            user = get_object_or_404(User, nickname=nickname)  # 닉네임으로 사용자 조회
-        except:
-            return Response({"error": "사용자를 찾을수 없습니다."}, status=404)
+
+        user = get_object_or_404(User, nickname=nickname)  # 닉네임으로 사용자 조회
+
         if user == request.user:  # 요청한 사용자가 본인인지 확인
             journals = user.communities_author.all()  # 사용자의 모든 커뮤니티 가져오기
             serializer = JournalSerializer(journals, many=True)
