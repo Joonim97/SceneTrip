6dcf4e9150db0139ba8bcbc9b80e3ebffe002c2e
HeureuseD <da4260@daum.net>
2024-10-08 17:47:51 +0900

    feat: ALLOWED_HOSTS 수정, STATIC_ROOT 추가. ALLOWED_HOSTS = [] -> ALLOWED_HOSTS = ['54.180.149.228', 'localhost', '127.0.0.1']

--- SceneTrip/settings.py
+++ SceneTrip/settings.py
@@ -51,7 +51,9 @@ MANAGER_EMAIL = get_secret("MANAGER_EMAIL")  # 관리자의 이메일 주소
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.180.149.228', 'localhost', '127.0.0.1']
+
+STATIC_ROOT = BASE_DIR / "static"
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),  # 액세스 토큰 만료 시간
