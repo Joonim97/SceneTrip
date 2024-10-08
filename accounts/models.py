0e7787db184ee0b56188be435e3dffa8e7e5235f
HeureuseD <da4260@daum.net>
2024-10-08 17:45:29 +0900

    feat: username 필드 추가 및 birth_date를 REQUIRED_FIELDS에 추가함. 배포 과정에서 superuser 생성 시 필요해서 추가함.

--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,7 @@ class User(AbstractUser):
     # 성, 이름은 username 으로 대체
     # first_name = models.CharField(max_length=150, blank=False) # 성
     # last_name = models.CharField(max_length=150, blank=False) # 이름
+    username = models.CharField(max_length=150, unique=True, blank=True, null=True)
     user_id = models.CharField(max_length=150, unique=True) # 아이디
     email = models.EmailField(unique=True, blank=False) # 이메일
     birth_date = models.DateField(blank=False) # 생년월일
@@ -32,4 +33,4 @@ class User(AbstractUser):
 
 
     USERNAME_FIELD = 'user_id'  # 사용자 이름으로 사용할 필드
-    REQUIRED_FIELDS = ['email']  # 필수로 입력해야 할 필드
+    REQUIRED_FIELDS = ['email', 'username', 'birth_date']  # 필수로 입력해야 할 필드
