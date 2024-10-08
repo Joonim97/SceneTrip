ee5f6ca6d2ba061b4715e766d35996a3d593142f
HeureuseD <da4260@daum.net>
2024-10-08 17:40:03 +0900

    fix: email 인증 시 IP주소 수정 중간 배포 후 IP주소 변경.

--- accounts/emails.py
+++ accounts/emails.py
@@ -4,7 +4,7 @@ from django.conf import settings
 # 회원가입 정보가 맞으면 이 메세지 출력
 def send_verification_email(user):
     subject = '이메일 인증을 완료해주세요'
-    message = f'안녕하세요 {user.username}님, 아래 링크를 클릭하여 이메일 인증을 완료해주세요.\n\n http://127.0.0.1:8000/api/accounts/verify/{user.verification_token}'
+    message = f'안녕하세요 {user.username}님, 아래 링크를 클릭하여 이메일 인증을 완료해주세요.\n\n http://54.180.149.228/api/accounts/verify/{user.verification_token}'
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [user.email]
     send_mail(subject, message, email_from, recipient_list)
@@ -19,14 +19,14 @@ def send_verification_email(user):
 
 def send_verification_email_reset(user):
     subject = '이메일 변경을 위한 인증을 완료해주세요'
-    message = f'안녕하세요 {user.username}님, 아래 링크를 클릭하여 이메일 변경을 위한 인증을 완료해주세요.\n\n http://127.0.0.1:8000/api/accounts/emailchange/verify/{user.verification_token}'
+    message = f'안녕하세요 {user.username}님, 아래 링크를 클릭하여 이메일 변경을 위한 인증을 완료해주세요.\n\n http://54.180.149.228/api/accounts/emailchange/verify/{user.verification_token}'
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [user.new_email]
     send_mail(subject, message, email_from, recipient_list)
 
 def send_verification_password_reset(user):
     subject = '비밀번호 변경을 위한 이메일인증을 완료해주세요'
-    message = f'안녕하세요 {user.username}님, 아래 링크를 클릭하여 비밀번호 변경을 위한 인증을 완료해주세요.\n\n http://127.0.0.1:8000/api/accounts/passwordchange/verify/{user.verification_token}'
+    message = f'안녕하세요 {user.username}님, 아래 링크를 클릭하여 비밀번호 변경을 위한 인증을 완료해주세요.\n\n http://54.180.149.228/api/accounts/passwordchange/verify/{user.verification_token}'
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [user.email]
     send_mail(subject, message, email_from, recipient_list)
