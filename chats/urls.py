9d9175bfdd893f48187fa227d90bdcbd7ad025b2
unknown <pigking41@gmail.com>
2024-10-14 10:20:14 +0900

    feat: 채팅기능 html로 가는 url

--- chats/urls.py
+++ chats/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import ChatRoomListCreateView, MessageListCreateView
 
-app_name = 'chats'
+from . import views
 
 urlpatterns = [
-    path('rooms/', ChatRoomListCreateView.as_view(), name='chat-room-list-create'),  # 채팅방 리스트 및 생성
-    path('rooms/<int:id>/messages/', MessageListCreateView.as_view(), name='message-list'), 
+    path('', views.index, name='index'),
+    path('<str:room_name>/', views.room, name='room'),
 ]
