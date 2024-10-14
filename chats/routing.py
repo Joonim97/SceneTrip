dc5998ade5914bb28226063b946294ad30526de1
unknown <pigking41@gmail.com>
2024-10-14 10:19:28 +0900

    feat: websockeet url 추가

--- chats/routing.py
+++ chats/routing.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls import re_path
 
-from chats import consumers
+from . import consumers
 
 websocket_urlpatterns = [
-    path("ws/room/<int:room_id>/messages", consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/api/chats/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
