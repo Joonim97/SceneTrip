2921fbc45dc2fc62888cb2cde363c227c65891ad
unknown <pigking41@gmail.com>
2024-10-14 10:21:52 +0900

    feat: 채팅기능

--- SceneTrip/asgi.py
+++ SceneTrip/asgi.py
@@ -1,33 +1,17 @@
-"""
-ASGI config for SceneTrip project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
 
+from channels.auth import AuthMiddlewareStack #추가
+from channels.routing import ProtocolTypeRouter, URLRouter #URLRouter 추가
 from django.core.asgi import get_asgi_application
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-import chats.routing  # 이제 이 코드는 안전하게 실행될 수 있습니다.
+import chats.routing # chat import
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SceneTrip.settings')
 
-django_asgi_app = get_asgi_application()
-
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": 
-        AuthMiddlewareStack(
-            AllowedHostsOriginValidator(
-            URLRouter(
-                chats.routing.websocket_urlpatterns
-            )     
-        ),
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack( # 추가
+        URLRouter(
+            chats.routing.websocket_urlpatterns
+        )
     ),
 })
