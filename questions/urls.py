d7c922642123f83171f7510057caed0c0c35b995
KimGyeongMinB <pigking41@gmail.com>
2024-10-22 11:59:06 +0900

    feat: 댓글 상세조회, 수정, 삭제

--- questions/urls.py
+++ questions/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import QnaListView, QnaWriteView, QuestionListView, QuestionDetailAPIView, CommentCreateView, CommentUpdateView,CommentDeleteView
+from .views import CommentView, QnaListView, QnaWriteView, QuestionListView, QuestionDetailAPIView
 from questions import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = "questions"
 
@@ -9,11 +11,11 @@ urlpatterns =  [
     path('', QuestionListView.as_view(), name='question-list'),
     # 큐앤에이 상세조회, 수정, 삭제
     path('<str:key>/', QuestionDetailAPIView.as_view(), name='question-detail'),
-
-    # 댓글 조회, 생성
-    path('<str:key>/comments/', CommentCreateView.as_view(), name='question-comment'),
+    # 대댓글
+    path('<str:key>/comments/<int:parent_id>/', CommentView.as_view(), name='question-reply'),
+    # 댓글 조회, 생성, 수정, 삭제
+    path('<str:key>/comments/', CommentView.as_view(), name='question-comment'),
     # 댓글 수정, 삭제
-    path('comments/<str:key>/', CommentUpdateView.as_view(), name='questions-comment-detatil'),
     path('qna/page/', QnaListView.as_view(), name='qna-list'), # Q&A 전체페이지
-    path('qna/page/write/', QnaWriteView.as_view(), name='qna_write') # Q&A 글 작성 페이지
-    ]
+    path('qna/page/write/', QnaWriteView.as_view(), name='qna_write'), # Q&A 글 작성 페이지
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
